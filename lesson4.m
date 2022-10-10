cbce4baabff07862a7ee142d7934aa0946b6980c
wangdaodaodao <ahking0516@126.com>
2022-10-10 21:15:04 +0800

    uu

--- lesson4.m
+++ lesson4.m
@@ -1,3 +1,13 @@
+//
+//  lesson4.m
+//  
+//
+//  Created by 王导导 on 2022/10/10.
+//第33-35课
+
+#import <Foundation/Foundation.h>
+
+
 /*
 1.NULL 
 可以作为指针变量的值,如果一个指针变量的值为NULL,说明这个指针不指向内存中的任何一块空间,等价于0;
@@ -8,13 +18,38 @@
         Person *p2 = nil;
 4.如果类指针为nil, 不能赋值和访问属性,也不能执行它的方法;
 
-
-
 */
 
 
 
+
+/*
+分组导航 
+
+#prama mark - 分组名
+*/
+
 #import <Foundation/Foundation.h>
+
+#prama mark - 猫猫类
+@interface cat : NSObject{
+
+}
+@end    
+
+@implementation cat
+@end
+
+
+#prama mark - 狗狗类
+@interface dog : NSObject{
+
+}
+@end    
+
+@implementation dog
+@end
+
 
 
 int main(){
---
+++ lesson5.m
@@ -0,0 +1,20 @@
+//
+//  lesson5.m
+//  
+//
+//  Created by 王导导 on 2022/10/7.
+//第37课 方法与函数
+
+
+
+
+/*
+1.函数 :viod test(){
+}
+
+
+
+
+
+*/
+#import <Foundation/Foundation.h>
